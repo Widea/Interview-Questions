1fec9ebb24f3d9ec8c2fd20acb84f4061b4a3318
Viveka <aggarwalviveka@gmail.com>
2015-04-21 10:34:28 -0400

    numberDivisibility.java
    This program takes bits from the user. With each bit entered the user gets an option of adding more.
    
    Once the required number of bits are added, the program checks and reports if the number is divisible by 3. 
    
    The program works on the concept of bit manipulation.

---
+++ numberDivisibility.java
@@ -0,0 +1,62 @@
+
+public class numberDivisibility{
+	
+static int number;	
+	
+public static void padNumber(int bitValue)	
+{
+	 number = (number << 1) | bitValue;
+}
+
+public static boolean isDivisibleBy3(){
+int n = number;
+	int oddCount = 0, evenCount = 0;
+	
+if(n < 0)   n = -n;
+if(n == 0) return true;
+if(n == 1) return false;
+
+while(n != 0)
+{
+   
+    if((n & 1) == 1) 
+       oddCount++;
+    n = n>>1;
+
+    if((n & 1) == 1)
+        evenCount++;
+    n = n>>1;
+}
+
+
+return ((Math.abs(oddCount - evenCount)) % 3 == 0 );	
+}	
+	
+	
+	
+public static void main(String[] args){
+	
+java.util.Scanner inputScanner = new java.util.Scanner(System.in);
+System.out.println();
+
+
+System.out.println();
+int input = 1;
+
+//System.out.println(number + " " + bitValue);
+while(input == 1){
+	System.out.print("Enter the bit value: ");
+	int bitValue = inputScanner.nextInt();
+	padNumber(bitValue);
+	
+	System.out.print("Do you want to continue adding bits(respond in 0/1)? :");
+	input = inputScanner.nextInt();
+}
+
+
+System.out.println("Is divisible by three: " +isDivisibleBy3());
+inputScanner.close();
+}	
+	
+}
+
