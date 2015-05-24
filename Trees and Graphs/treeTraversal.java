e6ea6742c1b749cc6d1e0f3cb73d81395323bcf1
Viveka <aggarwva@mail.uc.edu>
2015-05-24 19:42:02 -0400

    added BST search

--- Trees and Graphs/treeTraversal.java
+++ Trees and Graphs/treeTraversal.java
@@ -3,139 +3,144 @@
 // 1. adding nodes to the tree (displays message when duplicate node is input)
 // 2. tree traversals (in-order, pre-order, post-order, level-order)
 // 3. converts tree toString, in-order format
+// 4. searches for a given value in the BST
+
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class treeTraversal{
-	
-	node root;
-	
-	public class node
-	{
-		node left;
-		node right;
-		Object data;
-		
-		// default node constructor
-		// sets everything to null
-		public node()
-		{			
-		}
-		
-		// parameterized constructor
-		// sets the new node's data value to inputData
-		public node(int inputData)
-		{
-			data = inputData; 
-			left = null;
-			right = null;
-		}
-	}
-	
-	// default tree constructor
-	//creates a new root node and sets all its configs to null
-	public treeTraversal()
-	{
-		root = new node();
-	}
-	
-	// parameterized tree constructor
-	// create a tree node, with no children and null links, with value input
-	public treeTraversal(int input)
-	{
-		root = new node(input);
-	}
-	
-	// does not allow user to add duplicate values <binary tree rule>
-	// checks for the order of the input value and adds it at appropriate position. 
-	public void addToTree(int inputVal)
-	{
-		if (root == null)
-		{
-			root = new node(inputVal);
-			return;
-		}
-	
-		node temp = root;
-		node tempp;
-		while(true)
-		{
-			tempp = temp;
-			if((int)temp.data > inputVal)
-			{
-				temp = temp.left;
-				if(temp == null)
-				{
-					tempp.left = new node(inputVal);
-					return;
-				}				 
-			}
-			
-			else if((int)temp.data < inputVal)
-			{
-				temp = temp.right;
-				if(temp == null)
-				{
-					tempp.right = new node(inputVal);
-					return;
-				}		
-			}
-			
-			else
-			{
-				System.out.println("Input Value already exists, hence value: " +inputVal +" cannot be added to the binary tree");
-				return;
-			}			
-		}
-	}
-	
-	@Override
-	public String toString()
-	{
-	return toString(this.root);	
-	}
-	
-	// return in-order traversal string of the tree.
-	public String toString(node start)
-	{
-		 if (start == null) 
-		 {
-		        return null;
-		 }
-		 
-		 return "[" + toString(start.left) + "," + start.data + "," + toString(start.right) + "]";
-	}
-	
-	// function to implement pre-order traversal
-	public void preOrder(node start)
-	{
-		if (start != null) 
-		{		 
-			System.out.println(start.data);
-			preOrder(start.left);
-			preOrder(start.right);
-		}
-	}
-	
-	// function to implement in-order traversal
-	public void inOrder(node start)
-	{
-		if (start != null) 
-		{
-			inOrder(start.left);
-			System.out.println(start.data);
-			inOrder(start.right);
-		}
-	}
-	
-	// function to implement post-order traversal
-	public void postOrder(node start)
-	{
-		if (start != null) 
-		{
-			postOrder(start.left);
-			postOrder(start.right);
-			System.out.println(start.data);
-		}
-	}
-	
+    
+    node root;
+    
+    public class node
+    {
+        node left;
+        node right;
+        Object data;
+        
+        // default node constructor
+        // sets everything to null
+        public node()
+        {
+        }
+        
+        // parameterized constructor
+        // sets the new node's data value to inputData
+        public node(int inputData)
+        {
+            data = inputData;
+            left = null;
+            right = null;
+        }
+    }
+    
+    // default tree constructor
+    //creates a new root node and sets all its configs to null
+    public treeTraversal()
+    {
+        root = new node();
+    }
+    
+    // parameterized tree constructor
+    // create a tree node, with no children and null links, with value input
+    public treeTraversal(int input)
+    {
+        root = new node(input);
+    }
+    
+    // does not allow user to add duplicate values <binary tree rule>
+    // checks for the order of the input value and adds it at appropriate position.
+    public void addToTree(int inputVal)
+    {
+        if (root == null)
+        {
+            root = new node(inputVal);
+            return;
+        }
+        
+        node temp = root;
+        node tempp;
+        while(true)
+        {
+            tempp = temp;
+            if((int)temp.data > inputVal)
+            {
+                temp = temp.left;
+                if(temp == null)
+                {
+                    tempp.left = new node(inputVal);
+                    return;
+                }
+            }
+            
+            else if((int)temp.data < inputVal)
+            {
+                temp = temp.right;
+                if(temp == null)
+                {
+                    tempp.right = new node(inputVal);
+                    return;
+                }
+            }
+            
+            else
+            {
+                System.out.println("Input Value already exists, hence value: " +inputVal +" cannot be added to the binary tree");
+                return;
+            }
+        }
+    }
+    
+    @Override
+    public String toString()
+    {
+        return toString(this.root);
+    }
+    
+    // return in-order traversal string of the tree.
+    public String toString(node start)
+    {
+        if (start == null)
+        {
+            return null;
+        }
+        
+        return "[" + toString(start.left) + "," + start.data + "," + toString(start.right) + "]";
+    }
+    
+    // function to implement pre-order traversal
+    public void preOrder(node start)
+    {
+        if (start != null)
+        {
+            System.out.println(start.data);
+            preOrder(start.left);
+            preOrder(start.right);
+        }
+    }
+    
+    // function to implement in-order traversal
+    public void inOrder(node start)
+    {
+        if (start != null)
+        {
+            inOrder(start.left);
+            System.out.println(start.data);
+            inOrder(start.right);
+        }
+    }
+    
+    // function to implement post-order traversal
+    public void postOrder(node start)
+    {
+        if (start != null)
+        {
+            postOrder(start.left);
+            postOrder(start.right);
+            System.out.println(start.data);
+        }
+    }
+    
     // function to implement level-order traversal
     public void levelOrder(node start)
     {
@@ -159,21 +164,58 @@ public class treeTraversal{
             {
                 nodequeue.add(temp.right);
             }
-        }		
+        }
     }
-	
-	public static void main(String[] args)
-	{
-		treeTraversal bTree = new treeTraversal(5);
-		bTree.addToTree(1);
-		bTree.addToTree(4);
-		bTree.addToTree(4);
-		bTree.addToTree(3);
-		bTree.addToTree(7);
-		
-		System.out.println(bTree.toString());
-		System.out.println("***Pre-Order Traversal***"); bTree.preOrder(bTree.root);
-		System.out.println("***In-Order Traversal***"); bTree.inOrder(bTree.root);
-		System.out.println("***Post-Order Traversal***"); bTree.postOrder(bTree.root);
-	}
+    
+    // function implementing searching in BST
+    public boolean searchTree(Integer inputValue)
+    {
+        if(root == null || inputValue == null)
+            return false;
+        
+        node temp = root;
+        
+        while(temp != null)
+        {
+            if((Integer)temp.data == inputValue )
+                return true;
+            else if((Integer)temp.data > inputValue )
+                temp = temp.left;
+            else
+                temp = temp.right;
+        }
+        
+        return false;
+        
+    }
+    
+    public static void main(String[] args)
+    {
+        treeTraversal bTree = new treeTraversal(5);
+        bTree.addToTree(1);
+        bTree.addToTree(4);
+        bTree.addToTree(4);
+        bTree.addToTree(3);
+        bTree.addToTree(7);
+        
+        System.out.println(bTree.toString());
+        System.out.println("***Pre-Order Traversal***"); bTree.preOrder(bTree.root);
+        System.out.println("***In-Order Traversal***"); bTree.inOrder(bTree.root);
+        System.out.println("***Post-Order Traversal***"); bTree.postOrder(bTree.root);
+        System.out.println("***Level-Order Traversal***"); bTree.levelOrder(bTree.root);
+        
+        System.out.println();
+        System.out.println("***Searching***");
+        // negative test cases
+        System.out.println("Tree contains 22? " +bTree.searchTree(22));
+        System.out.println("Tree contains 10? " +bTree.searchTree(10));
+        System.out.println("Tree contains null? " +bTree.searchTree(null));
+        
+        // positive test cases
+        System.out.println();
+        System.out.println("Tree contains 1? " +bTree.searchTree(1));
+        System.out.println("Tree contains 5? " +bTree.searchTree(5));
+        System.out.println("Tree contains 7? " +bTree.searchTree(7));
+        
+    }
 }
