3465a2a837584270979568520128d43ab6ed97de
Viveka <aggarwalviveka@gmail.com>
2015-05-25 17:18:45 -0400

    Update first.java

--- Trees/Cracking the coding interview/first.java
+++ Trees/Cracking the coding interview/first.java
@@ -1,4 +1,4 @@
-// program to check if a given tree is balanced. 
+// Program to check if a given tree is balanced. 
 // A balanced tree is defined to be a tree such that the heights of the two subtrees of any node never differ by more than one.
 
 public class first{
