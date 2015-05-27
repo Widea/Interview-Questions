53b71d2f6f79893b2d9b8847d31b69118d5fe1ec
Viveka <aggarwalviveka@gmail.com>
2015-05-27 08:26:33 -0400

    Update - added test case

--- Trees/Cracking the coding interview/sixth.java
+++ Trees/Cracking the coding interview/sixth.java
@@ -1,6 +1,6 @@
 public class sixth{
 
-	public class node
+	public static class node
 	{
 		node left;
 		node right;
@@ -11,8 +11,7 @@ public class sixth{
 		{
 			this.data = data;
 			this.parent = parent;
-		}
-		
+		}		
 	}
 	
 	public static node findNext(node input)
@@ -38,8 +37,7 @@ public class sixth{
 			p = input.parent;
 		}
 		
-		return input;
-		
+		return input;		
 	}
 	
 	public static node leftMostNode(node in)
@@ -56,10 +54,33 @@ public class sixth{
 		
 		return in;
 	}
-	
-	
+		
 	public static void main(String[] args)
 	{
-
+		node head = new node(7, null);
+		node firstLeft = new node(5, head);
+		node firstRight = new node(9, head);
+		node second1Left = new node(3, firstLeft);
+		node second1Right = new node(6, firstLeft);
+		node second2Left = new node(8, firstRight);
+		node second2Right = new node(11, firstRight);
+		node thirdLeft = new node(2, second1Left);
+		node thirdRight = new node(4, second1Left);
+		node lastNode =new node(12, second2Right);
+		
+		// Tree creation
+		head.left = firstLeft;
+		head.right = firstRight;
+		firstLeft.left = second1Left;
+		firstLeft.right = second1Right;
+		firstRight.left = second2Left;
+		firstRight.right = second2Right;
+		second1Left.left = thirdLeft;
+		second1Left.right = thirdRight;
+		second2Right.right = lastNode;
+					
+		node temp = findNext(head);
+		
+		System.out.println(temp.data);
 	}
 }
