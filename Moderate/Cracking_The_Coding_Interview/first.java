55f396f44f155763ad50a584fdcdae5afb843013
widea <aggarwalviveka@gmail.com>
2015-08-01 11:35:59 -0400

    added solution first

---
+++ Moderate/Cracking_The_Coding_Interview/first.java
@@ -0,0 +1,35 @@
+package moderate;
+
+/*
+ * Cracking the coding interview 
+ * Chapter: Sorting and Searching
+ * Solution: 17.1
+ * Write a function to swap a number in place(that is, without temporary variables).
+ * Author: Viveka Aggarwal
+ */
+
+public class first {
+	
+	public static void swap1(int a, int b) {
+		a = a + b; 
+		b = a - b;
+		a = a - b;
+		
+		System.out.println(a + " " + b);
+	}
+	
+	public static void swap2(int a, int b) {
+		a = a ^ b; // a xor b
+		b = a ^ b; // a xor b xor b
+		a = a ^ b; // a xor b xor a 
+		
+		System.out.println(a + " " + b);
+	} 
+	
+	public static void main(String[] args) {
+		int x = 6;
+		int y = 5;
+		swap1(x,y);
+		swap2(x,y);
+	}
+}
