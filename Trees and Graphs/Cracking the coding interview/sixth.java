a80b0fc32a924eccabb05f2ded9539f8383c6363
Viveka <aggarwalviveka@gmail.com>
2015-11-02 11:55:00 -0500

    Update sixth.java

--- Trees and Graphs/Cracking the coding interview/sixth.java
+++ Trees and Graphs/Cracking the coding interview/sixth.java
@@ -1,3 +1,7 @@
+/*
+ * Write an algorithm to find the'next'node (i.e., in-order successor) of a given node in a binary search tree. 
+ * You may assume that each node has a link to its parent.
+ */
 public class sixth{
 
 	public static class node
