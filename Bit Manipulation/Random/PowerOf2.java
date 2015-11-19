4f8568fa2bbe89fe0e2fc5e6fa0e276d7a6115f1
Viveka <aggarwalviveka@gmail.com>
2015-11-19 18:14:49 -0500

    Update PowerOf2.java

--- Bit Manipulation/Random/PowerOf2.java
+++ Bit Manipulation/Random/PowerOf2.java
@@ -1,15 +1,14 @@
 // Program to check if a given number is a power of two
-
 public class PowerOf2 {
 	static boolean isPowerOf2(int input) {
 		if(input <= 0)
 			return false;
-		
-		return ((input & (input << 1)) == 0);
+
+		return ((input & (input - 1)) == 0);
 	}
 	
 	public static void main(String[] args) {
-		System.out.println(isPowerOf2(0));
+		System.out.println(isPowerOf2(10));
 		System.out.println(isPowerOf2(-16));
 		System.out.println(isPowerOf2(1));
 		System.out.println(isPowerOf2(128));
