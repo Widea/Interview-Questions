04b6ecebdf8f751e00c387ef28fe50a1b06292e0
Viveka <aggarwalviveka@gmail.com>
2016-10-25 19:20:18 -0400

    Update MinReplacements.java

--- Moderate/Random/MinReplacements.java
+++ Moderate/Random/MinReplacements.java
@@ -38,3 +38,24 @@ public class MinReplacements {
             }
     }
 }
+
+
+/*
+Alternate solution: Posted by SergeyTachenov
+
+public int integerReplacement(int n) {
+    int c = 0;
+    while (n != 1) {
+        if ((n & 1) == 0) {
+            n >>>= 1;
+        } else if (n == 3 || ((n >>> 1) & 1) == 0) {
+            --n;
+        } else {
+            ++n;
+        }
+        ++c;
+    }
+    return c;
+}
+
+*/
