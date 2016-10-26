395b3bb1839e297d73260bc5ee83aba54020278b
Viveka <aggarwalviveka@gmail.com>
2016-10-26 15:51:55 -0400

    Create PatchingArray.java

---
+++ Moderate/Random/PatchingArray.java
@@ -0,0 +1,31 @@
+/*
+Question: Given a sorted positive integer array nums and an integer n, add/patch elements to the array such that any number in range [1, n] inclusive can be formed by the sum of some elements in the array. Return the minimum number of patches required.
+
+Example 1:
+nums = [1, 3], n = 6
+Return 1.
+
+Source: LeetCode.
+*/
+
+public class PatchingArray
+{
+public int minPatches(int[] nums, int n) {
+	int sum = 1;
+	int i = 0;
+	int count = 0;
+	
+	while(sum <= n){
+		if(i < nums.length && nums[i] <= sum){
+			sum+=nums[i];
+      i++;
+		}
+		else{
+			sum += sum;
+			count++;
+		}
+	}
+	
+	return count;
+}
+}
